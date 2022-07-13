113dd6c38a9ee665ee16997c85ac7caccac88346
salcho-mirincom <salcho@mirincom.co.kr>
2022-07-13 14:36:16 +0900

    resutrn

---
+++ salcho_back/src/main/java/com/salcho/project/good/Application.java
@@ -0,0 +1,11 @@
+package com.salcho.project.good;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); // start whole Spring Framework
+    }
+}
---
+++ salcho_back/src/main/java/com/salcho/project/good/ReviewCtrl.java
@@ -0,0 +1,34 @@
+package com.salcho.project.good;
+
+import com.salcho.project.good.dao.ReviewDAO;
+import com.salcho.project.good.dto.ReviewDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/review")
+public class ReviewCtrl {
+    @Autowired
+    private ReviewDAO reviewDAO;
+
+    @PostMapping("/saveReview")
+    public void saveReview(@RequestBody ReviewDTO reviewDTO) {
+        String id = reviewDTO.getId();
+        if (id == null) {
+            String uuidStr = UUID.randomUUID().toString();
+            reviewDTO.setId(uuidStr);
+        }
+        reviewDAO.saveReview(reviewDTO);
+    }
+
+    @GetMapping("/getReviews")
+    public List<ReviewDTO> getReviews() { return reviewDAO.getReviews(); }
+
+    @DeleteMapping("/deleteReview")
+    public void deleteReview(@RequestParam(value = "id", required = true) String id) {
+        reviewDAO.deleteReview(id);
+    }
+}
